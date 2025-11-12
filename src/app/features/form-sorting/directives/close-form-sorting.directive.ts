e85eb1482ebf676c5284cabe864b072b0baf3fc5
Sergey Malyuga <sergey@malyuga.ru>
2025-11-12 10:23:13 +0300

    Реализовать закрытие формы сортировки.

---
+++ src/app/features/form-sorting/directives/close-form-sorting.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, EventEmitter, HostListener, Input, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appCloseFormSorting]'
+})
+export class CloseFormSortingDirective {
+  @Input({required: true}) isSortFormOpen!: boolean;
+  @Output() formSortClosed: EventEmitter<void> = new EventEmitter<void>();
+
+  @HostListener('mouseleave')
+  onMouseleave() {
+    if (this.isSortFormOpen) {
+      this.formSortClosed.emit();
+    }
+  }
+
+  @HostListener('keydown.escape')
+  onEscape() {
+    if (this.isSortFormOpen) {
+      this.formSortClosed.emit();
+    }
+  }
+}
--- src/app/features/form-sorting/form-sorting.component.ts
+++ src/app/features/form-sorting/form-sorting.component.ts
@@ -1,11 +1,13 @@
 import {ChangeDetectionStrategy, Component, signal, WritableSignal} from '@angular/core';
 import {ToggleFormSortDirective} from './directives/toggle-form-sorting.directive';
+import {CloseFormSortingDirective} from './directives/close-form-sorting.directive';
 
 @Component({
   selector: 'app-form-sorting',
   templateUrl: './form-sorting.component.html',
   imports: [
-    ToggleFormSortDirective
+    ToggleFormSortDirective,
+    CloseFormSortingDirective
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -14,5 +16,9 @@ export class FormSortingComponent {
 
   public onFormToggled() {
     this.isSortFormOpen.set(!this.isSortFormOpen())
+  }
+
+  public onFormSortClosed() {
+    this.isSortFormOpen.set(false);
   }
 }
