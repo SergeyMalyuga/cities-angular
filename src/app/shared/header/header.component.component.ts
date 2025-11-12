cc25170d5b6ef795388bc29fd98a4f23fc695b10
Sergey Malyuga <sergey@malyuga.ru>
2025-11-12 12:44:04 +0300

    Настроить отображение элементов интерфейса header.component в зависимости от статуса авторизации.

--- src/app/shared/header/header.component.component.ts
+++ src/app/shared/header/header.component.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal, WritableSignal} from '@angular/core';
+import {AppRoute, AuthorizationStatus} from '../../core/constants/const';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    RouterLink
+  ]
 })
-export class HeaderComponentComponent {}
+export class HeaderComponentComponent {
+  public authStatus: WritableSignal<AuthorizationStatus> = signal<AuthorizationStatus>(AuthorizationStatus.UN_AUTH);
+  protected readonly AuthorizationStatus = AuthorizationStatus;
+  protected readonly AppRoute = AppRoute;
+}
