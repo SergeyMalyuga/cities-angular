475895f6008a8fbfe2e187ef1eba537923078a31
Sergey Malyuga <sergey@malyuga.ru>
2025-11-18 13:04:30 +0300

    Реализовать переход на страницу выбранного оффера при клике на карточку оффера на главной странице.

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { OfferPreview } from '../../core/models/offers';
 import { CapitalizePipe } from '../pipes/capitilize.pipe';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../core/constants/const';
 
 @Component({
   selector: 'app-card',
-  imports: [CapitalizePipe],
+  imports: [CapitalizePipe, RouterLink],
   templateUrl: './card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
   @Input({ required: true }) offer!: OfferPreview;
   public readonly Math = Math;
+  protected readonly AppRoute = AppRoute;
 }
