7adb94a0e137420f45c7a41d450649e2d8d303c3
Sergey Malyuga <sergey@malyuga.ru>
2025-11-20 11:41:28 +0300

    Реализовать загрузку комментариев при переходе на страницу выбранного оффера.

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,10 +1,10 @@
-import {ChangeDetectionStrategy, Component, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
 import {HeaderComponent} from '../../shared/header/header.component';
 import {ActivatedRoute} from '@angular/router';
 import {Offer} from '../../core/models/offers';
 import {OfferService} from '../../core/services/offer.service';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {of, switchMap} from 'rxjs';
+import {forkJoin, of, switchMap} from 'rxjs';
 import {CapitalizePipe} from '../../shared/pipes/capitilize.pipe';
 import {Comment} from '../../core/models/comments';
 import {CommentService} from '../../core/services/comment.service';
@@ -19,26 +19,30 @@ export class OfferComponent implements OnInit {
   public offer = signal<Offer | null>(null);
   public offerId = signal<string | null>(null);
   public comments = signal<Comment[]>([]);
+  public commentsAmount = computed(() => this.comments.length);
   public readonly Math = Math;
   private offerService = inject(OfferService);
   private commentService = inject(CommentService);
   private activatedRouter = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
-  constructor() {
-    effect(() => {
-      const id = this.offerId();
-      if (id) {
-        this.commentService.getComments(id).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(comments => this.comments.set(comments));
-      }
-    });
-  }
-
   ngOnInit(): void {
     this.activatedRouter.paramMap.pipe(takeUntilDestroyed(this.destroyRef), switchMap(params => {
       const id = params.get('id');
+      if (id === null) {
+        return of(null);
+      }
       this.offerId.set(id);
-      return id ? this.offerService.getOfferById(id) : of(null)
-    })).subscribe((offer) => this.offer.set(offer));
+      return forkJoin({
+        offer: this.offerService.getOfferById(id),
+        comments: this.commentService.getComments(id),
+      })
+    })).subscribe((result) => {
+      if (result) {
+        this.offer.set(result.offer);
+        this.comments.set(result.comments);
+      }
+    });
   }
 }
+
